6df01d701f87a2d2571444ad065208fb3effafa4
Saarthak <zsaarthakz@gmail.com>
2021-10-27 17:47:05 +0530

    v1.0

--- index.js
+++ index.js
@@ -13,21 +13,21 @@ const tf = require("@tensorflow/tfjs-node");
   const maskImageDataSet = fs.readdirSync(maskImageDataSetURL);
   const noMaskImageDataSet = fs.readdirSync(noMaskImageDataSetURL);
 
-  for (let i = 0;i < maskImageDataSet.length;i++) {
-    let imageData = fs.readFileSync(maskImageDataSetURL + "/" + maskImageDataSet[i]);
-    let imageTensor = tf.node.decodeJpeg(imageData, 3);
-    let newImageTensor = tf.image.resizeBilinear(imageTensor, [50, 50]);
-    let newImageData = await tf.node.encodePng(newImageTensor);
-    fs.writeFileSync(`${maskImageNewDataSetURL}/${i}.jpg`, newImageData);
-  };
+  // for (let i = 0;i < maskImageDataSet.length;i++) {
+  //   let imageData = fs.readFileSync(maskImageDataSetURL + "/" + maskImageDataSet[i]);
+  //   let imageTensor = tf.node.decodeJpeg(imageData, 3);
+  //   let newImageTensor = tf.image.resizeBilinear(imageTensor, [50, 50]);
+  //   let newImageData = await tf.node.encodePng(newImageTensor);
+  //   fs.writeFileSync(`${maskImageNewDataSetURL}/${i}.jpg`, newImageData);
+  // };
 
-  for (let i = 0;i < noMaskImageDataSet.length;i++) {
-    let imageData = fs.readFileSync(noMaskImageDataSetURL + "/" + noMaskImageDataSet[i]);
-    let imageTensor = tf.node.decodePng(imageData, 3);
-    let newImageTensor = tf.image.resizeBilinear(imageTensor, [50, 50]);
-    let newImageData = await tf.node.encodePng(newImageTensor);
-    fs.writeFileSync(`${noMaskImageNewDataSetURL}/${i}.jpg`, newImageData);
-  };
+  // for (let i = 0;i < noMaskImageDataSet.length;i++) {
+  //   let imageData = fs.readFileSync(noMaskImageDataSetURL + "/" + noMaskImageDataSet[i]);
+  //   let imageTensor = tf.node.decodePng(imageData, 3);
+  //   let newImageTensor = tf.image.resizeBilinear(imageTensor, [50, 50]);
+  //   let newImageData = await tf.node.encodePng(newImageTensor);
+  //   fs.writeFileSync(`${noMaskImageNewDataSetURL}/${i}.jpg`, newImageData);
+  // };
 
   // Creating a model
   const model = tf.sequential();
@@ -56,47 +56,67 @@ const tf = require("@tensorflow/tfjs-node");
 
   model.compile({
     loss: "meanSquaredError",
-    optimizer: "sgd"
+    optimizer: tf.train.sgd(0.2)
   });
+
+  model.summary();
   console.log("Model Compiled");
 
   //Importing the new data
   const maskImageNewDataSet = fs.readdirSync(maskImageNewDataSetURL);
   const noMaskImageNewDataSet = fs.readdirSync(noMaskImageNewDataSetURL);
 
-  //Generating Mask image tensors and labels
-  let maskImageTensorsArr = [];
-  let maskImageLabelsArr = [];
+  let allImageTensors = [];
+  let allImageLabelTensors = [];
+
   for (let i = 0;i < maskImageNewDataSet.length;i++) {
     let imageData = fs.readFileSync(maskImageNewDataSetURL + "/" + maskImageNewDataSet[i]);
     let imageTensor = tf.node.decodeJpeg(imageData, 3);
-    maskImageTensorsArr.push(imageTensor);
-    maskImageLabelsArr.push(1);
+    let imageTensorArr = imageTensor.arraySync();
+    imageTensorArr.forEach((pixelArr) => pixelArr.forEach((pixel) => pixel.forEach((color, index) => pixel[index] = color / 255)));
+    imageTensor = tf.tensor3d(imageTensorArr);
+    allImageTensors.push(imageTensor);
+    allImageLabelTensors.push(1);
   };
-  const maskImageTensor = tf.stack(maskImageTensorsArr); //4D Tensor of Input images ( With Mask )
-  const maskImageLabelTensor = tf.oneHot(tf.tensor1d(maskImageLabelsArr, "int32"), 2);
 
-  //Generating No Mask image tensors and labels
-  let noMaskImageTensorsArr = [];
-  let noMaskImageLabelsArr = [];
   for (let i = 0;i < noMaskImageNewDataSet.length;i++) {
     let imageData = fs.readFileSync(noMaskImageNewDataSetURL + "/" + noMaskImageNewDataSet[i]);
     let imageTensor = tf.node.decodeJpeg(imageData, 3);
-    noMaskImageTensorsArr.push(imageTensor);
-    noMaskImageLabelsArr.push(0);
+    let imageTensorArr = imageTensor.arraySync();
+    imageTensorArr.forEach((pixelArr) => pixelArr.forEach((pixel) => pixel.forEach((color, index) => pixel[index] = color / 255)));
+    imageTensor = tf.tensor3d(imageTensorArr);
+    allImageTensors.push(imageTensor);
+    allImageLabelTensors.push(0);
   };
-  const noMaskImageTensor = tf.stack(noMaskImageTensorsArr); //4D Tensor of Input images ( Without Mask )
-  const noMaskImageLabelTensor = tf.oneHot(tf.tensor1d(noMaskImageLabelsArr, "int32"), 2);
 
-  //Fitting the model
-  await model.fit(maskImageTensor, maskImageLabelTensor, {
-    epochs: 100
-  });
+  const xs = tf.stack(allImageTensors);
+  const ys = tf.oneHot(tf.tensor1d(allImageLabelTensors, "int32"), 2);
 
-  await model.fit(noMaskImageTensor, noMaskImageLabelTensor, {
+  // Fitting the model
+  await model.fit(xs, ys, {
     epochs: 100
   });
 
   console.log("Training complete");
+
+  {
+    const testData = fs.readFileSync(maskImageNewDataSetURL + "/" + maskImageNewDataSet[0]);
+    let testImageTensor = tf.node.decodeJpeg(testData, 3);
+    let testImageTensorArr = testImageTensor.arraySync();
+    testImageTensorArr.forEach((pixelArr) => pixelArr.forEach((pixel) => pixel.forEach((color, index) => pixel[index] = color / 255)));
+    testImageTensor = tf.tensor3d(testImageTensorArr);
+    const prediction = await model.predict(tf.stack([testImageTensor]));
+    prediction.print();
+  }
+
+  {
+    const testData = fs.readFileSync(noMaskImageNewDataSetURL + "/" + noMaskImageNewDataSet[0]);
+    let testImageTensor = tf.node.decodeJpeg(testData, 3);
+    let testImageTensorArr = testImageTensor.arraySync();
+    testImageTensorArr.forEach((pixelArr) => pixelArr.forEach((pixel) => pixel.forEach((color, index) => pixel[index] = color / 255)));
+    testImageTensor = tf.tensor3d(testImageTensorArr);
+    const prediction = await model.predict(tf.stack([testImageTensor]));
+    prediction.print();
+  }
 
 })();
